d2b7cbdd1d752be7fe3c195932e14f1bc8031d71
Mark Sayers <marksayers46@icloud.com>
2020-07-08 20:41:06 -0500

    started todoform.js state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 
+
+const data = [
+  {
+    task: 'Make todo list',
+    id: 1234,
+    completed: false
+  }
+]
 class App extends React.Component {
-  
   // You will need a place to store your state in this component.
   // Design `App` to be the parent component of your application.
   // This component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      // add my state object
+      todo: data,
+      lastTodo: data,
+      searchText: ''
+    };
+  };
+  
   render() {
+    console.log('rendering...')
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <div clasName='App'>
+        <h1 className='title'>Welcome to your Todo App!</h1>
+      
+      </div>  
     );
   }
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class TodoForm extends React.Componenet {
+  constructor() {
+    super();
+    this.state = {
+      task: ''
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+export default TodoForm;
